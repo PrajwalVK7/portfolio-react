ea4218493b78767cb93336115c2f791e62b4e8ff
PrajwalVK7 <prajwalvk98@gmail.com>
2023-12-15 18:09:02 +0530

    started project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import "./Project.css"
-import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
-import { Row,Col } from 'react-bootstrap';
 function Projects() {
   return (
    <>
    <h1>Portfolio & PreviousProjects</h1>
-    
-
    </>
   )
 }
